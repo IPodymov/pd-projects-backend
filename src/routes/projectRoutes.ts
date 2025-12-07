c95ad8dcfbe246c86bddfec8eab468a9c23b9de6
Ivan Podymov <p0dymovivan@yandex.ru>
2025-12-07 18:23:35 +0300

    Feat: Allow admins to create projects

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -10,12 +10,31 @@ const router = Router();
 router.get("/", [checkJwt], ProjectController.listAll);
 router.get("/:id", [checkJwt], ProjectController.getOne);
 
-// Student
-router.post("/", [checkJwt, checkRole([UserRole.STUDENT])], ProjectController.createProject);
-router.post("/:id/join", [checkJwt, checkRole([UserRole.STUDENT])], ProjectController.joinProject);
-router.post("/:id/upload", [checkJwt, upload.single("file")], ProjectController.uploadFile);
+// Student & Admin
+router.post(
+  "/",
+  [checkJwt, checkRole([UserRole.STUDENT, UserRole.ADMIN])],
+  ProjectController.createProject
+);
+router.post(
+  "/:id/join",
+  [checkJwt, checkRole([UserRole.STUDENT])],
+  ProjectController.joinProject
+);
+router.post(
+  "/:id/upload",
+  [checkJwt, upload.single("file")],
+  ProjectController.uploadFile
+);
 
 // Teacher / Staff / Admin
-router.patch("/:id/status", [checkJwt, checkRole([UserRole.TEACHER, UserRole.UNIVERSITY_STAFF, UserRole.ADMIN])], ProjectController.updateStatus);
+router.patch(
+  "/:id/status",
+  [
+    checkJwt,
+    checkRole([UserRole.TEACHER, UserRole.UNIVERSITY_STAFF, UserRole.ADMIN]),
+  ],
+  ProjectController.updateStatus
+);
 
 export default router;
