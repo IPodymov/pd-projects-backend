76d9e8c20022c5c3eac2de28b9342fb6b1819fc9
Ivan Podymov <p0dymovivan@yandex.ru>
2025-12-07 16:48:16 +0300

    Fix: Add root health check route and update auth middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,10 @@ import project from "./projectRoutes";
 
 const routes = Router();
 
+routes.get("/", (req, res) => {
+    res.send({ message: "PD Projects Backend is running" });
+});
+
 routes.use("/auth", auth);
 routes.use("/users", user);
 routes.use("/projects", project);
