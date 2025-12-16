c5f180e4b1861b35ea8cf89f0285cb77ca821471
Ivan Podymov <p0dymovivan@yandex.ru>
2025-12-16 19:08:05 +0300

    style: format code for consistency and readability

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,40 +1,41 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
-import { ProjectsService } from './projects.service';
-import { CreateProjectDto } from './dto/create-project.dto';
-import { UpdateProjectDto } from './dto/update-project.dto';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { OptionalJwtAuthGuard } from '../auth/optional-jwt-auth.guard';
+import {Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards} from '@nestjs/common';
+import {ProjectsService} from './projects.service';
+import {CreateProjectDto} from './dto/create-project.dto';
+import {UpdateProjectDto} from './dto/update-project.dto';
+import {JwtAuthGuard} from '../auth/jwt-auth.guard';
+import {OptionalJwtAuthGuard} from '../auth/optional-jwt-auth.guard';
 
 @Controller('projects')
 export class ProjectsController {
-  constructor(private readonly projectsService: ProjectsService) {}
+    constructor(private readonly projectsService: ProjectsService) {
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Post()
-  create(@Body() createProjectDto: CreateProjectDto, @Req() req) {
-    return this.projectsService.create(createProjectDto, req.user);
-  }
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    create(@Body() createProjectDto: CreateProjectDto, @Req() req) {
+        return this.projectsService.create(createProjectDto, req.user);
+    }
 
-  @UseGuards(OptionalJwtAuthGuard)
-  @Get()
-  findAll(@Req() req) {
-    return this.projectsService.findAll(req.user);
-  }
+    @UseGuards(OptionalJwtAuthGuard)
+    @Get()
+    findAll(@Req() req) {
+        return this.projectsService.findAll(req.user);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.projectsService.findOne(+id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.projectsService.findOne(+id);
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto, @Req() req) {
-    return this.projectsService.update(+id, updateProjectDto, req.user);
-  }
+    @UseGuards(JwtAuthGuard)
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto, @Req() req) {
+        return this.projectsService.update(+id, updateProjectDto, req.user);
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.projectsService.remove(+id);
-  }
+    @UseGuards(JwtAuthGuard)
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.projectsService.remove(+id);
+    }
 }
